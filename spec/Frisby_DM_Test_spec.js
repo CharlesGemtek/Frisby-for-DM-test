15e81b032dc20c4223ca16cd3afd58b709ed1bd3
charles_huang <charles_huang@gemteks.com>
2015-05-27 11:30:44 +0800

    Add get_firmware_list API test

--- spec/Frisby_DM_Test_spec.js
+++ spec/Frisby_DM_Test_spec.js
@@ -1,43 +1,30 @@
 describe('HTTP Method Test Suite', function() {
     var frisby = require('frisby');
 
+    //Test get_dm_ip API
     frisby.create('Get Device Manager IP')
-          .get('https://localhost:8080/web/v2/client/get_dm_ip',
-             { strictSSL: false /* options to pass to request go here */})
-          .expectStatus(200)
-          .expectHeaderContains('content-type', 'text/html; charset=utf-8')
-          .expectBodyContains('https')
-          .inspectBody()
-          .toss();
+        .get('https://localhost:8080/web/v2/client/get_dm_ip',
+            { strictSSL: false /* options to pass to request go here */})
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'text/html; charset=utf-8')
+        .expectBodyContains('https')
+        .toss();
 
-/*    frisby.create('POST Method')
-          .post('http://localhost:8080')
-          .expectStatus(200)
-          .expectHeaderContains('content-type', 'application/json')
-          .expectJSON({
-              method : 'POST'
-          })
-          .toss();
+    //Test get_firmware_list API with no key option
+    frisby.create('Get Device Manager IP')
+        .get('https://localhost:8080/web/v2/client/get_firmware_list',
+            { strictSSL: false})
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'text/html; charset=utf-8')
+        //Test get_firmware_list API with key option,and get key from previous test's res
+        .afterJSON(function(res) {
+            frisby.create('Second test, run after first is completed')
+                .get('https://localhost:8080/web/v2/client/get_firmware_list?key='+res[0].model_name,
+                  { strictSSL: false})
+                .expectStatus(200)
+                .expectHeaderContains('content-type', 'text/html; charset=utf-8')
+            .toss()
+        })
+        .toss();
 
-    frisby.create('PUT Method')
-          .put('http://localhost:8080')
-          .expectStatus(200)
-          .expectHeaderContains('content-type', 'application/json')
-          .expectJSON({
-              method : 'PUT'
-          })
-          .expectJSONTypes({
-              method : String
-          }).toss();
-
-    frisby.create('DELETE Method')
-          .delete('http://localhost:8080')
-          .expectStatus(200)
-          .expectHeaderContains('content-type', 'application/json')
-          .expectJSON({
-              method : 'DELETE'
-          })
-          .expectJSONTypes({
-              method : String
-          }).toss();*/
 });
